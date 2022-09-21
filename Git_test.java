4593c4ae4e72a0e7903a3c57a4e4487d4a979ec9
Viji7789 <94017112+Viji7789@users.noreply.github.com>
2022-09-21 17:28:47 -0400

    Update Git_test.java
    first_change_after_commit

--- Git_test.java
+++ Git_test.java
@@ -4,6 +4,7 @@ public class Git_test {
 
 	public static void main(String[] args) {
 System.out.println("This is jenkins integration with Git");
+		System.out.println("This is added after initial commit");
 	}
 
 }
